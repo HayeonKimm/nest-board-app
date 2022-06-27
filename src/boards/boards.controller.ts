60f4e7e1626d548a7f1231ded1b670815ce474ee
hayeonkimm <hayeon.data@gmail.com>
2022-06-28 08:39:50 +0900

    업데이트 - all file
    변경사항 :

---
+++ src/boards/boards.controller.ts
@@ -0,0 +1,13 @@
+import { BoardsService } from './boards.service';
+import { Controller, Get } from '@nestjs/common';
+import { Board } from './boards.model';
+
+@Controller('boards')
+export class BoardsController {
+  constructor(private boardsService: BoardsService) {}
+
+  @Get()
+  getAllBoards(): Board[] {
+    return this.boardsService.getAllBoards();
+  }
+}
